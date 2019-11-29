6ebe6912c0fbf2cd5dc44c178b8b8058372251be
Glitch (react-app-template) <none>
2019-11-29 00:16:41 +0000

    📼🚧 Checkpoint ./src/components/NewTimer.js:303291/4761 ./src/index.css:303291/155 ./src/components/Timer.js:303291/9240

--- src/components/NewTimer.js
+++ src/components/NewTimer.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const getSeconds = (hours, minutes, seconds) => {
   return hours * 3600 + minutes * 60 + seconds;
-}
+};
 
 export const NewTimer = ({ timers, setTimers }) => {
   const [hours, setHours] = useState(0);
@@ -10,12 +10,46 @@ export const NewTimer = ({ timers, setTimers }) => {
   const [seconds, setSeconds] = useState(0);
 
   return (
-    <div className="timer-inputs">
-      <input type="number" name="Hours" min="0" max="60" onChange={(e) => setHours(e.target.value)}></input>
-      <input type="number" name="Minutes" min="0" max="60" onChange={(e) => setMinutes(e.target.value)}></input>
-      <input type="number" name="Seconds" min="0" max="60" onChange={(e) => setSeconds(e.target.value)}></input>
-      
-      <button className="button" onClick={() => setTimers([...timers, getSeconds(hours, minutes, seconds)])}>
+    <div className="new-timer">
+      <div>
+        <input
+          type="number"
+          name="Hours"
+          placeholder="00"
+          min="0"
+          max="60"
+          onChange={e => setHours(e.target.value)}
+        ></input>
+        hours
+      </div>
+      <div>
+        <input
+          type="number"
+          name="Minutes"
+          placeholder="00"
+          min="0"
+          max="60"
+          onChange={e => setMinutes(e.target.value)}
+        ></input>
+        minutes
+      </div>
+      <div>
+        <input
+          type="number"
+          name="Seconds"
+          placeholder="00"
+          min="0"
+          max="60"
+          onChange={e => setSeconds(e.target.value)}
+        ></input>
+        seconds
+      </div>
+
+      <button
+        onClick={() =>
+          setTimers([...timers, getSeconds(hours, minutes, seconds)])
+        }
+      >
         Add Timer
       </button>
     </div>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,16 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const getTime = (totalSeconds) => {
+const audio = new AudioContext();
+
+const beep = () => {
+  const oscillator = audio.createOscillator();
+  const gain = audio.createGain();
+  oscillator.connect(gain);
+  oscillator.frequency.value = 400;
+  oscillator.type = "square";
+  gain.connect(audio.destination);
+  gain.gain.value = 5 * 0.01;
+  oscillator.start(audio.currentTime);
+  oscillator.stop(audio.currentTime + 1);
+  v.start(audio.currentTime);
+  v.stop(audio.currentTime + 1);
+  v.start(audio.currentTime);
+  v.stop(audio.currentTime + 1);
+};
+
+const getTime = totalSeconds => {
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = Math.floor((totalSeconds % 3600) % 60);
   return [hours, minutes, seconds];
-}
+};
 
 export const Timer = ({ initialSeconds }) => {
   const [seconds, setSeconds] = useState(initialSeconds);
   const [isRunning, setIsRunning] = useState(true);
-  
+
   const [hh, mm, ss] = getTime(seconds);
 
   useEffect(() => {
@@ -18,8 +36,15 @@ export const Timer = ({ initialSeconds }) => {
     if (isRunning) {
       interval = setInterval(() => {
         setSeconds(seconds - 1);
+
+        if (seconds <= 0) {
+          beep();
+          setSeconds(0);
+          setIsRunning(false);
+          clearInterval(interval);
+        }
       }, 1000);
-    } else if (!isRunning && seconds !== 0) {
+    } else if (!isRunning && seconds <= 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
@@ -28,18 +53,26 @@ export const Timer = ({ initialSeconds }) => {
   return (
     <div className="app">
       <div className="time">
-        {hh ? `${hh} h ` : ''}
-        {mm ? `${mm} min ` : ''}
-        {ss ? `${ss} sec` : ''}
+        {hh ? `${hh} h ` : ""}
+        {mm ? `${mm} min ` : ""}
+        {ss ? `${ss} sec` : ""}
       </div>
       <div className="row">
-        <button className={`button button-primary button-primary-${isRunning ? 'active' : 'inactive'}`} onClick={() => setIsRunning(!isRunning)}>
-          {isRunning ? 'Pause' : 'Start'}
+        <button
+          className={`button button-primary button-primary-${
+            isRunning ? "active" : "inactive"
+          }`}
+          onClick={() => setIsRunning(!isRunning)}
+        >
+          {isRunning ? "Pause" : "Start"}
         </button>
-        <button className="button" onClick={() => {
+        <button
+          className="button"
+          onClick={() => {
             setSeconds(0);
             setIsRunning(false);
-          }}>
+          }}
+        >
           Reset
         </button>
       </div>
